fc5ffc7c9337ce25d0ade407b7834452be217406
Purush <purushottam.yadav@yellowmessenger.com>
2021-07-27 17:26:23 +0530

    Added demo application files

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { filter } from 'minimatch';
+
+declare let cordova: any;
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,22 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  private ymChat;
+  private botId: String;
 
-  constructor() {}
+  constructor() {
+    this.ymChat = cordova.plugins.ymchat;
+    this.botId = "x1608615889375";
+    cordova.plugins.ymchat.setBotId(this.botId);
+  }
 
+  startYmChatBot() {
+    cordova.plugins.ymchat.startBot(success, failure);
+  }
 }
+const success = (result) => {
+  alert(result);
+};
+const failure = (result) => {
+  alert(result);
+};
